c267e28ef29906e2db6daed4cbeb309b6ae7a612
Elliot Burin and James Osborn <elliot@>
2017-09-12 10:25:11 -0700

    add pictures and slidetoggle feature

---
+++ walrus/js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#initially-showing").slideToggle();
+    $("#initially-hidden").slideToggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable2").click(function() {
+    $("#initially-showing2").slideToggle();
+    $("#initially-hidden2").slideToggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable3").click(function() {
+    $("#initially-showing3").slideToggle();
+    $("#initially-hidden3").slideToggle();
+  });
+});
