ac568e1c1534256c7f511cb6765de019292c4460
Elliot Burin and James Osborn <james@>
2017-09-12 11:15:32 -0700

    add button functions to change color of background and text.

--- walrus/js/scripts.js
+++ walrus/js/scripts.js
@@ -3,18 +3,25 @@ $(document).ready(function() {
     $("#initially-showing").slideToggle();
     $("#initially-hidden").slideToggle();
   });
-});
 
-$(document).ready(function() {
   $(".clickable2").click(function() {
     $("#initially-showing2").slideToggle();
     $("#initially-hidden2").slideToggle();
   });
-});
 
-$(document).ready(function() {
   $(".clickable3").click(function() {
     $("#initially-showing3").slideToggle();
     $("#initially-hidden3").slideToggle();
+  });
+
+  $("button#light").click(function() {
+    $("body").removeClass();
+    $("body").addClass("light-background");
+  });
+
+  $("button#dark").click(function() {
+    $("body").removeClass();
+    $("body").addClass("dark-background");
+    $("body").addClass("light-text")
   });
 });
